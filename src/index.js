b6b82076aed805de5f7fa8dfa2921bcf37c592b3
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-02-26 11:43:59 +0530

    Setup default bodyparser

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const setupAndStartServer = async () => {
 
     // create the express object
     const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
     });
