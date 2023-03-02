471cd21d49036b7a999d5f34bb847e7d863659db
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-03-02 12:01:49 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 
 const db = require("./models/index");
-const { City, Airport } = require("./models/index");
+// const { City, Airport, Airplane } = require("./models/index");
 
 const setupAndStartServer = async () => {
 
@@ -19,41 +19,10 @@ const setupAndStartServer = async () => {
     app.listen(PORT, async () => {
         console.log(`Server started at port ${PORT}`);
 
-        // const airports = await Airport.findAll({
-        //     include: [
-        //         {
-        //             model: City
-        //         }
-        //     ]
-        // });
-        // console.log(airports);
-
-        // const airports = await City.findAll({
-        //     where: {
-        //         id: 15
-        //     },
-        //     include: [
-        //         {
-        //             model: Airport
-        //         }
-        //     ]
-        // });
-        // console.log(airports);
-
         if (process.env.SYNC_DB) {
             db.sequelize.sync({ alter: true });
         }
-        // const city = await City.findOne({
-        //     where: {
-        //         id: 15
-        //     }
-        // });
-        // const airports = await city.getAirports();
-        // await city.addAirport({
-        //     name: 'Jindal Vijaynagar Airport',
-        //     cityId: 12
-        // });
-        // console.log(airports);
+
     });
 
 }
